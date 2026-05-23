be0b2b57ca3901a21f9129056645cdd43562d280
Arunrsingh61 <arunsingh220111077@gmail.com>
2026-05-23 08:54:19 +0530

    updated torch version

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ encoder = VGGEncoder('vgg_normalised.pth').to(device)
 decoder = Decoder().to(device)
 decoder.load_state_dict(
     torch.load(
-        'NST_Code/experiment/final_exp/decoder_final.pth',
+        'experiment/final_exp/decoder_final.pth',
         map_location=torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
     )
 )
